ff54e53d868f3d6c5f58f03e10e7e5828a1874db
Nafissah Pouye <Nafissah Pouye>
2017-10-18 04:04:26 +0000

    build the charts

--- js/site.js
+++ js/site.js
@@ -3,11 +3,19 @@ function generateringComponent(vardata, vargeodata){
   var lookup = genLookup(vargeodata) ;
   var cmrMap = dc.leafletChoroplethChart('#Map');
   var ue_trend = dc.compositeChart('#agency');
-  var sectors = dc.rowChart('#sector');
+  var rr_trend = dc.compositeChart('#sector');
+  var whoChart = dc.rowChart('#who');
+  var whatChart = dc.rowChart('#what');
   var cf = crossfilter(vardata) ;
   var all = cf.groupAll();
   var mapDimension = cf.dimension(function(d) { return d.rowcacode1});
   var mapGroup = mapDimension.group().reduceSum(function(d){ return d.approved});
+  var allocDim = cf.dimension(function(d) {
+    return d.allocation
+  });
+
+  //console.log(crossfilter(allocDim.filter("RR")));
+
   //begin test
   var scale_maxDate = new Date(2016, 3, 10);
   var numberFormat = d3.format(',f');
@@ -22,43 +30,86 @@ function generateringComponent(vardata, vargeodata){
         e.date = dateFormat.parse(e.date);
     });
 
-var colors = ['#FAE61E','#FAE61E','#E67800','#C80000','#E6E6FA', '#023858', '#a6bddb','#3690c0'] ;
+var colors = ['#FAE61E','#03a9f4','#E67800','#C80000','#E6E6FA', '#023858', '#a6bddb','#3690c0'] ;
   
-var dateDimension = cf.dimension(function (d) { return d.date});
+var dateDimension = cf.dimension(function (d) { return d.Year;});
+console.log(dateDimension);
 
-  var groupApproved = dateDimension.group().reduceSum(function (d){
+var groupApproved_ufe = dateDimension.group().reduceSum(function (d){return d.approved/1000000;});
+function remove_space(groupApproved_rr) { 
+  return {
+    all: function() {
+      
+      return groupApproved_rr.all().filter(function(d) {
+        console.log(d)
+        return d.value != "";
+      });
+    }
+  };
+}
 
-    if(isNaN(d.approved)){console.log('Not included: ');console.log(d);return 0;} else {return d.approved / 1000;}
+var groupApproved_rr = dateDimension.group().reduceSum(function (d){ return d.approved_rr/1000000;});
 
-  });
   var sectorDimension = cf.dimension(function (d){return d.sector});
   var sectorGroup = sectorDimension.group();
-  ue_trend
-            .width(300)
-            .height(230)
-            .dimension(dateDimension)
-            .x(d3.time.scale().domain([new Date(2013, 11, 0), new Date(2017, 3, 31)]))
-            .elasticY(true)
-            .legend(dc.legend().x($('#Requirement').width()-150).y(0).gap(3))
-            .valueAccessor(function(d){return d.value.avg != "";})
-            .shareTitle(false)
-            //.omit("")
-            .compose([
-                dc.lineChart(ue_trend).group(groupApproved, 'Approved UE funding').colors(colors[5]).title(function (d) { return [dateFormatPretty1(d.key),  "Food Sec Req: " + numberFormat(d.value) + ' Million US $'].join('\n'); }),
-               // dc.lineChart(req_trends).group(groupFundings, 'Funding').colors(colors[6]).title(function (d) { return [dateFormatPretty1(d.key), "Funding: " + numberFormat(d.value) + ' Million US $'].join('\n'); }),
-              ])
-            .margins({top: 20, right: 0, bottom: 30, left: 60})
-            .brushOn(false)
-            .renderHorizontalGridLines(true)
-            .renderTitle(true)
-            .xAxisLabel("Date")
-            .xAxis().ticks(3)
-            ue_trend.yAxis().tickFormat(function (v) {
-            return v + 'M';
-        });
-
+  var whatDimension = cf.dimension(function (d){return d.Agency});
+  var whatGroup = whatDimension.group();
+    // compositeChart
+     ue_trend.width(350)
+               .height(370)
+               .dimension(dateDimension)
+               .x(d3.scale.linear().domain([2008, 2017]))
+               //.x(d3.time.scale().domain([new Date(2005, 11, 0), new Date(2018, 3, 31)]))
+               .legend(dc.legend().x($('#agency').width()-5).y(0).gap(3))
+               .shareTitle(false)
+               .valueAccessor(function(d){return d.key != "";})
+               .compose([
+                 dc.lineChart(ue_trend).group(groupApproved_ufe, "UFE").colors(colors[1]).title(function (p) {
+                   return ["Année         : " + p.key , "Financement UFE : " + numberFormat(p.value) + "k" ].join('\n'); }),/*.renderDataPoints({radius: 2, fillOpacity: 0.8, strokeOpacity: 0.8}),*/
+              
+                 
+                  ])
+               .label(function (p) { return p.key; })
+               /*.title(function (d) {
+                   return ["Année      : " + d.key , "Biomasse : " + d.value + " k" ].join('\n'); })*/
+               .margins({top: 10, right: 13, bottom: 80, left: 30})
+               .brushOn(false)
+              // .renderTitle(true)
+               .elasticX(false)
+               .elasticY(true)
+               .colorAccessor(function(d,i){ return 0;});
+               ue_trend.xAxis().tickFormat(d3.format("d"));
+              /* .renderlet(function (chart) {
+                    chart.selectAll("g.x text")
+                      .attr('dx', '-12')
+                      .attr('transform', "rotate(-60)");
+                });*/
+ rr_trend.width(350)
+               .height(370)
+               .dimension(dateDimension)
+               .x(d3.scale.linear().domain([2008, 2017]))
+               //.x(d3.time.scale().domain([new Date(2005, 11, 0), new Date(2018, 3, 31)]))
+               .legend(dc.legend().x($('#sector').width()-5).y(0).gap(3))
+               .shareTitle(false)
+               .valueAccessor(function(d){return d.key != "";})
+               .compose([
+                 dc.lineChart(rr_trend).group(groupApproved_rr, "RR").colors(colors[1]).title(function (p) {
+                   return ["Année      : " + p.key , "Financement RR : " + numberFormat(p.value) + "M" ].join('\n'); }),/*.renderDataPoints({radius: 2, fillOpacity: 0.8, strokeOpacity: 0.8}),*/
+              
+                 
+                  ])
+               .label(function (p) { return p.key; })
+               /*.title(function (d) {
+                   return ["Année      : " + d.key , "Biomasse : " + d.value + " k" ].join('\n'); })*/
+               .margins({top: 10, right: 13, bottom: 80, left: 30})
+               .brushOn(false)
+              // .renderTitle(true)
+               .elasticX(true)
+               .elasticY(false)
+               .colorAccessor(function(d,i){ return 0;});
+        rr_trend.xAxis().tickFormat(d3.format("d"));
 //sector rowChart
-sectors.width($('sector').width()).height(400)
+whoChart.width($('sector').width()).height(300)
             .dimension(sectorDimension)
             .group(sectorGroup)
             .elasticX(true)
@@ -66,9 +117,20 @@ sectors.width($('sector').width()).height(400)
                 return group.top(15);
             })
             .labelOffsetY(13)
-            .colors(colors[4])
+            .colors(colors[1])
             .colorAccessor(function(d, i){return 0;})
-            .xAxis().ticks(5);
+           // .xAxis().ticks(5);
+whatChart.width($('sector').width()).height(300)
+            .dimension(whatDimension)
+            .group(whatGroup)
+            .elasticX(true)
+            .data(function(group) {
+                return group.top(15);
+            })
+            .labelOffsetY(13)
+            .colors(colors[1])
+            .colorAccessor(function(d, i){return 0;})
+            //.xAxis().ticks(5);
 
   //end test
 
@@ -85,7 +147,6 @@ dc.dataCount('#count-info')
              .center([0,0])
              .zoom(0)
              .geojson(vargeodata)
-             .colors(['#DDDDDD','#FFC8BF','#F59181', '#FE5A43', '#921301', '#620D00'])
              .colors(['#CCCCCC','#03a9f4'])
              .colorDomain([0,1])
              .colorAccessor(function (d){
